983d4f6c0b12d311f58085de84a3eb1f0c69de53
Neroan <69721386+NeroanIsAfk@users.noreply.github.com>
2023-04-20 23:38:31 +0200

    Ajout des packages + début de JavaFX

--- Java/Foodify/src/Main.java
+++ Java/Foodify/src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main extends Application {
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("userInterfacePackage/MainWindow.fxml")));
+        primaryStage.setTitle("Les gros pieds de logan");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+
     }
 }
