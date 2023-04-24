b6550d8fe0f26bb76933fe0e3596031e3dbcca18
Neroan <69721386+NeroanIsAfk@users.noreply.github.com>
2023-04-24 14:57:55 +0200

    Main window ( without SQL Link ) + start of Register.fxml

--- Java/Foodify/src/Main.java
+++ Java/Foodify/src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,12 +14,13 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws IOException
+    {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("userInterfacePackage/MainWindow.fxml")));
-        primaryStage.setTitle("Les gros pieds de logan");
+        primaryStage.setTitle("Foodify");
+        primaryStage.getIcons().add(new Image("/images/Foodify.png"));
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
-
     }
 }
